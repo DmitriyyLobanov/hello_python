3ead93e33dbe376909a260b013fe06f52e93ff65
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-03-15 23:07:53 +0400

    Task18 - 19

---
+++ hello_python/Task18.py
@@ -0,0 +1,18 @@
+# Реализовать алгоритм перемешивания списка. 
+# Придумать алгоритм самостоятельно - не вышло, взял код из сети и "пропустил через тетрадь".
+
+from random import randrange
+
+def sattolo_cycle(items):
+    i = len(items)
+    while i > 1:
+        i = i - 1
+        j = randrange(i)  # 0 <= j <= i-1
+        items[j], items[i] = items[i], items[j]
+    return items
+
+
+list = [1, 2, 3, 4, 5]
+print(f'Начальный список: {list}.')
+print(f'Перемешанный список: {sattolo_cycle(list)}.')
+
