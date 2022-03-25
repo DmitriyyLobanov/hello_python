0388857cb550fe0ea8b3e6d3b2e058442ba1be8c
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-03-25 23:26:12 +0400

    Task19 - test distribution added

--- hello_python/Task19.py
+++ hello_python/Task19.py
@@ -4,6 +4,7 @@ from random import shuffle
 import time
 
 def stupid_accident(start, stop):
+    """Генератор псевдослучайных чисел: (начало диапазона, конец диапазона)"""
     sequence = [items for items in range(start, stop + 1)]
     seconds = str(time.time())
     index = int(seconds[len(seconds) - (len(str(stop)) - 1): ])
@@ -11,4 +12,11 @@ def stupid_accident(start, stop):
     return sequence[index]
     
 
-print(stupid_accident(1, 100))
+print(stupid_accident(1, 10))
+
+test_distribution = [stupid_accident(0, 10) for i in range(10000)]
+for i in range(11):
+    print(f'{i}: {test_distribution.count(i)}')
+    
+
+# print(test_distribution)
